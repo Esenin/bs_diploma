b15c518a0512582ffb72dd23e6b3c367cfb08804
Esenin <senin-i-i@ya.ru>
2016-05-15 14:50:44 +0300

    paper was updated after editors correction, tex_compile minor updates. Move paper to eps graphics. Separate tikz figures generation

--- tex_compile.sh
+++ tex_compile.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+rm -f diploma.aux diploma.out diploma.pdf diploma.bbl;
 xelatex diploma.tex && bibtex diploma.aux && xelatex -interaction=nonstopmode diploma.tex && xelatex -interaction=nonstopmode diploma.tex && echo Done || (echo "Exit"; echo Error)
 
